522015b1ea9f2ab69d38206ca1aa1adb5fc51135
TheVisual <132447890+TheVisual@users.noreply.github.com>
2023-06-28 00:36:58 -0500

    Init Commit

---
+++ YuboLib/REST/Endpoints/SignEndpoint.cs
@@ -0,0 +1,74 @@
+﻿using SnapchatLib;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using YuboLib.Exceptions;
+using YuboLib.Extras;
+
+namespace YuboLib.REST.Endpoints;
+internal interface ISignEndpoint
+{
+    Task<string> SignRequest();
+}
+
+internal class SignEndpoint : EndpointAccessor, ISignEndpoint
+{
+    internal const string DefaultSignUrl = "https://private.signer.sh/yubo/sign";
+
+    public SignEndpoint(YuboClient client, IYuboHttpClient httpClient, YuboLockedConfig config, IClientLogger logger, IUtilities utilities, IRequestConfigurator configurator) : base(client, httpClient, config, logger, utilities, configurator)
+    {
+    }
+
+    internal static HttpResponseMessage response { get; set; }
+    private void RaiseForInvalidValues()
+    {
+
+        
+        if (string.IsNullOrEmpty(Config.android_id) || string.IsNullOrEmpty(Config.ro_build_id) || string.IsNullOrEmpty(Config.ro_build_version_release) || string.IsNullOrEmpty(Config.ro_product_brand) || string.IsNullOrEmpty(Config.ro_product_model))
+            throw new Exception("Phone Stuff is required");
+
+        if (string.IsNullOrWhiteSpace(Config.android_id))
+        {
+            throw new AndroidIDNotSet();
+        }
+    }
+    public async Task<string> SignRequest()
+    {
+        RaiseForInvalidValues();
+
+        var request = new HttpRequestMessage(HttpMethod.Post, DefaultSignUrl);
+        request.Version = HttpVersion.Version20;
+        request.VersionPolicy = HttpVersionPolicy.RequestVersionExact;
+        request.Headers.TryAddWithoutValidation("x-auth", Config.ApiKey);
+        var sign_json = new SerializeSign { 
+            device = new Device 
+            { 
+                android_id = Config.android_id,
+                ro_build_id = Config.ro_build_id,
+                ro_build_version_release = Config.ro_build_version_release,
+                ro_product_brand = Config.ro_product_brand,
+                ro_product_model = Config.ro_product_model,
+            },
+            request = new Request 
+            { 
+                timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
+                username = Config.Username
+            }
+        };
+        request.Content = new StringContent(m_Utilities.JsonSerializeObject(sign_json), Encoding.UTF8, "application/json");
+
+
+
+        var responseData = await response.Content.ReadAsStringAsync();
+        if (response.StatusCode != HttpStatusCode.OK)
+            throw new SignerException(responseData);
+
+        if (responseData != null)
+            return responseData;
+
+        throw new SignerException(responseData);
+    }
+}
---
+++ YuboLib/REST/RequestConfigurator.cs
@@ -0,0 +1,147 @@
+﻿using SnapchatLib;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using YuboLib.Exceptions;
+using YuboLib.Extras;
+
+namespace YuboLib.REST;
+
+internal struct RequestConfiguration
+{
+    public string Endpoint;
+    public HttpMethod HttpMethod;
+    public bool IsMulti;
+}
+
+internal struct EndpointInfo
+{
+    public string BaseEndpoint;
+    public string Url;
+    public string SignUrlOverride = null;
+
+    public EndpointInfo()
+    {
+        BaseEndpoint = RequestConfigurator.ApiBaseEndpoint;
+        Url = "";
+    }
+}
+
+internal interface IRequestConfigurator
+{
+    Task<HttpRequestMessage> Configure(EndpointInfo endpointInfo, HttpContent content, HttpMethod httpMethod, YuboClient client, IYuboHttpClient httpClient, bool isMulti = false);
+    Task<HttpRequestMessage> Configure(EndpointInfo endpointInfo, Dictionary<string, string> parameters, HttpMethod httpMethod, YuboClient client, IYuboHttpClient httpClient, bool isMulti = false);
+}
+
+internal class RequestConfigurator : IRequestConfigurator
+{
+    internal static string ApiBaseEndpoint => "https://app.snapchat.com";
+    internal static string AcceptEncodingHeaderName => "Accept-Encoding";
+    internal static string AcceptHeaderName => "Accept";
+    internal static string AcceptLanguageHeaderName => "Accept-Language";
+    internal static string AcceptLocaleHeaderName => "Accept-Locale";
+    internal static string AcceptLanguageValue => "en";
+    internal static string AcceptLocaleValue => "en_US";
+    internal static string ApplicationJsonValue => "application/json";
+
+
+    private readonly IUtilities m_Utilities;
+    private readonly IClientLogger m_Logger;
+
+    public RequestConfigurator(IClientLogger logger, IUtilities utilities)
+    {
+        m_Logger = logger;
+        m_Utilities = utilities;
+    }
+
+    public async Task<HttpRequestMessage> Configure(EndpointInfo endpointInfo, HttpContent content, HttpMethod httpMethod, YuboClient client, IYuboHttpClient httpClient, bool isMulti = false)
+    {
+        var config = CreateConfig(endpointInfo, httpMethod, client, isMulti);
+        return await GenerateRequest(httpClient, config, endpointInfo, content);
+    }
+
+    public async Task<HttpRequestMessage> Configure(EndpointInfo endpointInfo, Dictionary<string, string> parameters, HttpMethod httpMethod, YuboClient client, IYuboHttpClient httpClient, bool isMulti = false)
+    {
+        var config = CreateConfig(endpointInfo, httpMethod, client, isMulti);
+        return await GenerateRequest(httpClient, config, endpointInfo, parameters, isMulti);
+    }
+
+    private RequestConfiguration CreateConfig(EndpointInfo endpointInfo, HttpMethod httpMethod, YuboClient client, bool isMulti = false)
+    {
+        var config = new RequestConfiguration
+        {
+            Endpoint = endpointInfo.Url,
+            HttpMethod = httpMethod,
+            IsMulti = isMulti,
+        };
+
+        return config;
+    }
+
+    private async Task<HttpRequestMessage> CreateRequest(IYuboHttpClient client, RequestConfiguration configuration, EndpointInfo endpointInfo)
+    {
+        var baseEndpoint = endpointInfo.BaseEndpoint;
+        var url = baseEndpoint + configuration.Endpoint;
+        var request = new HttpRequestMessage(configuration.HttpMethod, url);
+        request.Version = configuration.HttpMethod == HttpMethod.Put ? HttpVersion.Version11 : HttpVersion.Version20;
+        request.VersionPolicy = HttpVersionPolicy.RequestVersionExact;
+        return request;
+    }
+
+    private async Task<HttpRequestMessage> GenerateRequest(IYuboHttpClient client, RequestConfiguration configuration, EndpointInfo endpointInfo, HttpContent content)
+    {
+        var request = await CreateRequest(client, configuration, endpointInfo);
+
+        request.Content = content;
+        return request;
+    }
+
+    private async Task<HttpRequestMessage> GenerateRequest(IYuboHttpClient client, RequestConfiguration configuration, EndpointInfo endpointInfo, Dictionary<string, string> parameters, bool ismulti)
+    {
+        var request = await CreateRequest(client, configuration, endpointInfo);
+
+        // We only sign on POST
+        var signResult = m_Utilities.JsonDeserializeObject<SignJson>(await client.Sign.SignRequest());
+
+        m_Logger.Debug("Trying to add sign headers to request");
+        request.Headers.UserAgent.Clear();
+        if (signResult == null)
+            throw new SignerException("Could not deserialize SignRequest response");
+
+        request.Headers.TryAddWithoutValidation("x-brand-encoded", signResult.headers.XBrandEncoded);
+        request.Headers.TryAddWithoutValidation("x-device-model-encoded", signResult.headers.XDeviceModelEncoded);
+        request.Headers.TryAddWithoutValidation("x-carrier-encoded", signResult.headers.XCarrierEncoded);
+        request.Headers.TryAddWithoutValidation("x-android-version", signResult.headers.XAndroidVersion);
+        request.Headers.TryAddWithoutValidation("x-os", signResult.headers.XOS);
+        request.Headers.TryAddWithoutValidation("x-is-rooted", signResult.headers.XIsRooted);
+        request.Headers.TryAddWithoutValidation("x-is-emulator", signResult.headers.XIsEmulator);
+        request.Headers.TryAddWithoutValidation("accept-language", signResult.headers.AcceptLanguage);
+        request.Headers.TryAddWithoutValidation("user-agent", signResult.headers.UserAgent);
+        request.Headers.TryAddWithoutValidation("x-errors", signResult.headers.XErrors);
+        request.Headers.TryAddWithoutValidation("x-yellow-token", signResult.headers.XYellowToken);
+
+
+        parameters.Add("none", signResult.request_body.nonce);
+        parameters.Add("signature", signResult.request_body.signature);
+        if (ismulti)
+        {
+            var content = new MultipartFormDataContent();
+            foreach (var parameter in parameters)
+            {
+                Console.WriteLine(parameter.Key);
+                content.Add(new StringContent(parameter.Value), parameter.Key);
+            }
+            request.Content = content;
+        }
+        else
+        {
+            var content = new FormUrlEncodedContent(parameters);
+            content.Headers.ContentType.CharSet = "utf-8";
+            request.Content = content;
+        }
+
+        return request;
+    }
+}
